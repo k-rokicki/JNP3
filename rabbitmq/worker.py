4fb177e45e22c2f42d50d1c8ddca39d3911b62b4
Alek <alek.plocharski@gmail.com>
2020-01-20 14:18:51 +0100

    functions added to worker

--- rabbitmq/worker.py
+++ rabbitmq/worker.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 import pika
 import time
+from PIL import Image
+from os import system
+
+
+def resize_image(path_to_file, new_height, path_to_resized_file=None):
+    if path_to_resized_file is None:
+        path_to_resized_file = path_to_file
+
+    image = Image.open(path_to_file)
+    new_width = float(image.size[1]) * (new_height / image.size[0])
+    image.thumbnail((new_height, new_width), Image.ANTIALIAS)
+    image.save(path_to_resized_file, "JPEG")
+
+
+def copy_file_to_docker_container(file_path, docker_file_path, docker_tag):
+    system(f'sudo docker cp {file_path} {docker_tag}:{docker_file_path}')
+
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
@@ -12,7 +29,7 @@ print(' [*] Waiting for messages. To exit press CTRL+C')
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
-    time.sleep(body.count(b'.'))
+
     print(" [x] Done")
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
